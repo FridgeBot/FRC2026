d91a1ffad0d833d131f25eddf7e91cd2e972a71d
FridgeBot <corunna5084@gmail.com>
2026-01-19 15:57:47 -0500

    Intake Subsystem

--- MainRobot/src/main/java/frc/robot/RobotContainer.java
+++ MainRobot/src/main/java/frc/robot/RobotContainer.java
@@ -12,14 +12,22 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.Intake;
 
 public class RobotContainer {
+
+    //New Intake object called using the intake class.
+    private final Intake intakeSubsystem = new Intake();
+
+
+
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
@@ -32,7 +40,7 @@ public class RobotContainer {
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    private final CommandXboxController joystick = new CommandXboxController(0);
+    private final CommandJoystick joystick = new CommandJoystick(0);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
@@ -59,23 +67,29 @@ public class RobotContainer {
             drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+        joystick.button(99).whileTrue(drivetrain.applyRequest(() -> brake));
+        joystick.button(98).whileTrue(drivetrain.applyRequest(() ->
+            point.withModuleDirection(new Rotation2d(-joystick.getY(), -joystick.getX()))
         ));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        joystick.button(11).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        joystick.button(12).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        joystick.button(13).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        joystick.button(14).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // Reset the field-centric heading on left bumper press.
-        joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+        joystick.button(97).onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
 
         drivetrain.registerTelemetry(logger::telemeterize);
-    }
+    
+        //When button 2 is pressed, run the intake--
+        joystick.button(2).onTrue(intakeSubsystem.runIntake());
+        
+        }
+
+
 
     public Command getAutonomousCommand() {
         // Simple drive forward auton
---
+++ MainRobot/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Intake extends SubsystemBase{
+
+
+    //Motor Intake code begins here.
+
+    // Referencing motor controller object-
+    private final Spark m_motor = new Spark(0);
+
+    public Command runIntake(){
+        return startEnd(()-> m_motor.set(1), ()-> m_motor.set(0));
+
+    }
+
+
+
+
+
+
+
+
+
+}
