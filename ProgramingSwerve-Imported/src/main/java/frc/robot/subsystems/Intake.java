e3cb897695d34b569a12fed80ba34304c777ddf9
Nicholas Mars <nicholasrubenmars@gmail.com>
2026-02-13 09:34:14 -0500

    Add velocity control based command for shooter, and system identification for tuning

--- ProgramingSwerve-Imported/src/main/java/frc/robot/subsystems/Intake.java
+++ ProgramingSwerve-Imported/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,13 +1,25 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
+
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
-
+import edu.wpi.first.units.measure.MutAngle;
+import edu.wpi.first.units.measure.MutAngularVelocity;
+import edu.wpi.first.units.measure.MutVoltage;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 // 8ft and 2.44 meters with the power given to the motors
 
@@ -22,7 +34,7 @@ public class Intake extends SubsystemBase{
     
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("RPM", getM_motorSpeed());
+        SmartDashboard.putNumber("Current RPM", getM_motorSpeed());
     }
     
     
@@ -38,11 +50,54 @@ public class Intake extends SubsystemBase{
         return s_Encoder.getVelocity();
     }
 
-   public void m_motorSpeed(double speed){
-    m_motor.set(speed);
-   }
+    public void m_motorSpeed(double speed){
+        m_motor.set(speed);
+    }
 
-   public void s_motorSpeed(double speed){
-    s_motor.set(speed);
-   }
+    public void s_motorSpeed(double speed){
+        s_motor.set(speed);
+    }
+
+    public void m_motorVoltage(Voltage voltage){
+        m_motor.setVoltage(voltage);
+    }
+
+
+      // Mutable holder for unit-safe voltage values, persisted to avoid reallocation.
+  private final MutVoltage m_appliedVoltage = Volts.mutable(0);
+  // Mutable holder for unit-safe linear distance values, persisted to avoid reallocation.
+  private final MutAngle m_angle = Radians.mutable(0);
+  // Mutable holder for unit-safe linear velocity values, persisted to avoid reallocation.
+  private final MutAngularVelocity m_velocity = RadiansPerSecond.mutable(0);
+
+      // Create a new SysId routine for characterizing the shooter.
+  private final SysIdRoutine m_sysIdRoutine =
+      new SysIdRoutine(
+          // Empty config defaults to 1 volt/second ramp rate and 7 volt step voltage.
+          new SysIdRoutine.Config(),
+          new SysIdRoutine.Mechanism(
+              // Tell SysId how to plumb the driving voltage to the motor(s).
+              this::m_motorVoltage,
+              // Tell SysId how to record a frame of data for each motor on the mechanism being
+              // characterized.
+              log -> {
+                // Record a frame for the shooter motor.
+                log.motor("shooter-wheel")
+                    .voltage(
+                        m_appliedVoltage.mut_replace(m_motor.getAppliedOutput() * m_motor.getBusVoltage(), Volts))
+                    .angularPosition(m_angle.mut_replace(m_encoder.getPosition(), Rotations))
+                    .angularVelocity(
+                        m_velocity.mut_replace(m_encoder.getVelocity(), RotationsPerSecond));
+              },
+              // Tell SysId to make generated commands require this subsystem, suffix test state in
+              // WPILog with this subsystem's name ("shooter")
+              this));
+
+    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+        return m_sysIdRoutine.quasistatic(direction);
+    }
+
+    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+        return m_sysIdRoutine.dynamic(direction);
+    }
 }
