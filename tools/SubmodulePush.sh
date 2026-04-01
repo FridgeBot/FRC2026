3350b988cdc57da2c7507c161e94e6417e67d6d1
corunnaStudent5084 <nicholasrubenmars+student5084@gmail.com>
2026-03-31 20:03:14 -0700

    tools

---
+++ tools/SubmodulePush.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+read -p "Pick one SubModule by the first letter lowercase: " Flags
+
+case $Flags in 
+    o)
+    color="Orange"
+    ;;
+    b)
+
+    color="Black"
+    ;;
+    *)
+    exit
+    ;;
+esac
+
+
+read -p "commit Message: " Message
+
+cd ~/Desktop/FRC2026/MainRobot/src/main/java/frc/robot/submodule/
+
+git add --all
+
+    git commit -m "$Message"
+
+    git push -u origin $color
+
+    cd ~/Desktop/FRC2026/MainRobot/src
+
+    git add main/java/frc/robot/submodule
+
+    git commit -m "Submodule update after commit: $Messaage"
+
+    git push
